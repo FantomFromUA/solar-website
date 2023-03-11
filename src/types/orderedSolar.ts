6ce782a257986fc28db70f2d401c8c19f964b9a8
Erik <erik.toroni@gmail.com>
2023-03-11 16:40:21 +0100

    Solar page

---
+++ src/types/orderedSolar.ts
@@ -0,0 +1,29 @@
+import Solar from "../classes/Solar";
+
+export enum SolarTypes{
+    FETCH_SOLARS = "FETCH_SOLARS",
+    FETCH_SOLARS_SUCCESS = "FETCH_SOLARS_SUCCESS",
+    FETCH_SOLARS_ERROR = "FETCH_SOLARS_ERROR"
+}
+
+interface FetchSolarAction{
+    type: SolarTypes.FETCH_SOLARS;
+}
+
+interface FetchSolarSuccessAction{
+    type: SolarTypes.FETCH_SOLARS_SUCCESS;
+    payload: any[];
+}
+
+interface FetchSolarErrorAction{
+    type: SolarTypes.FETCH_SOLARS_ERROR;
+    payload: string;
+}
+
+export type SolarAction = FetchSolarAction | FetchSolarErrorAction | FetchSolarSuccessAction;
+
+export interface SolarState{
+    solars : Solar[],
+    loading: boolean,
+    error: null | string
+}
